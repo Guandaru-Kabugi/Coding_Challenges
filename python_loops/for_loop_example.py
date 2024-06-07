e93588f545c7dfefed80e21c4233fa9ca55b5e6e
Guandaru-Kabugi <kabugi96@gmail.com>
2024-06-07 11:42:06 +0300

    minor changes

--- python_loops/for_loop_example.py
+++ python_loops/for_loop_example.py
@@ -3,13 +3,11 @@ student_heights = input("What are the heights?").split()
 print(student_heights)
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-  
-# Write your code below this row 👇
+
 number_of_students = 0
 for n in range (len(student_heights)):
   number_of_students+=1
-#print(number_of_students)
+
 sum = 0
 for student_height in student_heights:
   sum+=student_height
