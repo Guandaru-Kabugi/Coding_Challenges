4477117267f896c963db7cef6c55c32c4ba093f5
Guandaru-Kabugi <kabugi96@gmail.com>
2024-06-16 16:42:25 +0300

    Blackjack Project

---
+++ Blackjack Capstone Project/version1.py
@@ -0,0 +1,18 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+playing = input("Do you want to play a game of blackjack? Type 'y' for yes and 'n' for no\n")
+My_cards = []
+computer_card = []
+if playing == 'y':
+    for number in range (2):
+        random_card = random.choice(cards)
+        My_cards.append(random_card)
+    results = My_cards[0] + My_cards[1]
+    print(f"Your cards are {My_cards}, current score: {results}")
+    for number in range (2):
+        random_comp_cards = random.choice(cards)
+        computer_card.append(random_card)
+    results_comp = computer_card[0] + computer_card[1]
+    print(f"Computer first hand: {computer_card[0]}")
+    print(f"Computer second hand: {computer_card[1]}")
+    print(results_comp)
---
+++ Blackjack Capstone Project/version4.py
@@ -0,0 +1,41 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+playing = input("Do you want to play a game of blackjack? Type 'y' for yes and 'n' for no\n")
+My_cards = []
+computer_card = []
+results = 0
+results_comp = 0
+if playing == 'y':
+    for number in range (2):
+        random_card = random.choice(cards)
+        My_cards.append(random_card)
+    for card in My_cards:
+        results+=card
+    print(f"Your cards are {My_cards}, current score: {results}")
+    for number in range (2):
+        random_comp_cards = random.choice(cards)
+        computer_card.append(random_comp_cards)
+    for card in computer_card:
+        results_comp+= card
+    # print(computer_card)
+    print(f"Computer first hand: {computer_card[0]}")
+    print (f"Computer second hand: {computer_card[1]}")
+    print(results_comp)
+continue_playing = True
+while continue_playing:
+    Another_card = input('Type "y" to get another card, type "n" to pass:\n')
+    if Another_card == "y":
+        random_card = random.choice(cards)
+        My_cards.append(random_card)
+        print(f"Your cards are: {My_cards}")
+        results+=random_card
+        print(f"Your score: {results}")
+        random_comp_cards = random.choice(cards)
+        computer_card.append(random_comp_cards)
+        results_comp+=random_comp_cards
+        print(f"Computer cards are: {computer_card}")
+        print(f"Computer score: {results_comp}")
+    elif Another_card == "n":
+        continue_playing = False
+        print(f"Your cards are: {My_cards}, final score: {results}")
+        print(f"Computer cards are: {computer_card}, final score: {results_comp}")
