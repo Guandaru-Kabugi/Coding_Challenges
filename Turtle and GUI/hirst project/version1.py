7ed61fb0d21d74c5d2c2db10d1b84a9fb3c741e6
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-02 14:41:49 +0300

    extracting colors from images

---
+++ Turtle and GUI/hirst project/version1.py
@@ -0,0 +1,24 @@
+import turtle as t
+tim = t.Turtle()
+tim.forward(100)
+
+#color_list = [(234, 35, 108), (237, 74, 34), (154, 27, 61), (6, 148, 94), (215, 229, 234), (234, 168, 39), (183, 158, 45), (26, 126, 194), (43, 191, 230), (85, 27, 92), (253, 223, 0), (125, 193, 73), (241, 219, 61), (180, 40, 101), (65, 175, 100), (212, 131, 166), (211, 58, 29)]
+
+
+
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
+
+# import colorgram
+# colors = colorgram.extract('Turtle and GUI/hirst project/image.jpg', 20)
+# color_list = []
+# for color in colors:
+#     rgb = color.rgb
+#     color_tuple = tuple(rgb)
+#     color_list.append(color_tuple)
+# print(color_list)
